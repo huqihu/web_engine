a5fc8316c04018f835ddc19b84aa2a1ee32faa6d
huqihu <1677056418@qq.com>
2020-11-16 21:48:20 +0800

    20201106

--- include/SplitTool.h
+++ include/SplitTool.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "myhead.h"
 #include "Mylogger.h"
-
+// 注意 这里的include 路径是 相对于 这个文件的路径,即使这个文件被 不在一个路劲的.c文件包含 
+//也不会出错( .c 文件在编译的时候 ,会使用相对路径叠加 起来 访问到 这个 .h 文件包含的头文件)
+// 但是 这里的 词典的文件路径 ,是在运行的时候访问的 ,并不是 包含在这个 头文件中的 , 是相对于运行的程序的路径
 #include "../cppjieba/include/cppjieba/Jieba.hpp"
 
 const char *const DICT_PATH = "../../cppjieba/dict/jieba.dict.utf8";
